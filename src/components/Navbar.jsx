6c29f85e50920cf7b32e776493f556ec87408d36
dahianamanzanares <dahiana.manzanares@anima.edu.uy>
2026-05-21 19:39:20 -0300

    Cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Brandsmart</a>
         <button 
           className="navbar-toggler" 
           type="button" 
